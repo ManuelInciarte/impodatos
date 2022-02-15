5d6a980f91f38f921cf55807b025bb3eb95c9cbf
manuel.inciarte <manuel.inciarte@movalto.com>
2022-02-15 18:11:51 -0500

    se agrega dashboard

---
+++ src/app/components/dashboard/dashboard-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { DashboardRoutingModule } from './dashboard.module';
+
+
+
+
+const routes: Routes = [
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class DashboardRoutingModule { }
