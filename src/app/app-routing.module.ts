b25b5a76af34c905ba0c030f33e4485179c77bc0
manuel.inciarte <manuel.inciarte@movalto.com>
2022-02-15 18:12:20 -0500

    Se agrega dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
-import { UploaderComponent } from './components/uploader/uploader.component';
+
 import { userSession } from './shared/guard/userSession.guard';
 
 const routes: Routes = [
-  {path: '', component: LoginComponent},
-  {path: 'uploader', component: UploaderComponent, canActivate : [userSession]}
+  {path: '', redirectTo: 'login', pathMatch:'full'},
+  {path: 'login', component: LoginComponent},
+  {path: 'uploader', component: UploaderComponent, canActivate : [userSession]},
+  {path: '**', redirectTo: 'login', pathMatch:'full'},
 ];
 
 @NgModule({
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
-import { UploaderComponent } from './uploader/uploader.component';
 import { SharedModule } from '../shared/shared.module';
 
 
@@ -12,7 +11,7 @@ import { SharedModule } from '../shared/shared.module';
 @NgModule({
   declarations: [
     LoginComponent,
-    UploaderComponent,    
+        
   ],
   imports: [
     
